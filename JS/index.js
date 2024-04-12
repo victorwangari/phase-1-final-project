a606d4f6b972e743b841a86e310f40cf601ed864
Victor kamau <Victor Wangari>
2024-04-13 02:37:32 +0300

    initial commit

---
+++ JS/index.js
@@ -0,0 +1,10 @@
+// diplaying nav bar
+
+const bar = document.getElementsByClassName('navbar-toggler');
+
+function openMenu() {
+    bar.style.right = "0"
+}
+function closeMenu(){
+    bar.style.right = "-200px"
+}
